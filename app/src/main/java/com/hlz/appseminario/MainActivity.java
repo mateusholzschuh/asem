5e4c5d7364d6720267c7af66c91f258f99be3471
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2018-12-13 00:08:46 -0200

    init

---
+++ app/src/main/java/com/hlz/appseminario/MainActivity.java
@@ -0,0 +1,33 @@
+package com.hlz.appseminario;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText text;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        text = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
+                alert.setTitle("Você clicou");
+                alert.setMessage(text.getText().toString());
+                alert.show();
+            }
+        });
+    }
+}
